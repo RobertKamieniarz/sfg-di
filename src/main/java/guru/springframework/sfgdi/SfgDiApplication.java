e5320e7ff4ea249e980b2190de79c3078e8369ea
RobertKamieniarz <robeo12055@gmail.com>
2022-12-05 17:12:06 +0100

    spring-di

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,36 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConsctuctorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String gretting = myController.sayHello();
+		System.out.println(gretting);
+
+		System.out.println("------- Property");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------ Constructor");
+
+		ConsctuctorInjectedController consctuctorInjectedController = (ConsctuctorInjectedController) ctx.getBean("consctuctorInjectedController");
+		System.out.println(consctuctorInjectedController.getGreeting());
 	}
 
 }
---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConsctuctorInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsctuctorInjectedControllerTest {
+    ConsctuctorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConsctuctorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
